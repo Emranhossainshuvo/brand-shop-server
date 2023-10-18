22a9169977ebf10b60567df252a359d564dce250
emran <shuvoorba28@gmail.com>
2023-10-18 22:45:36 +0600

    env file and gitignore file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,12 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-//  F7GSyr5kZIu5duTt
-// Filmic
 
 
-
-const uri = "mongodb+srv://Filmic:F7GSyr5kZIu5duTt@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
