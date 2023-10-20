df8757a2d5561dd5f1ca8fc6c7061e6784debf49
emran <shuvoorba28@gmail.com>
2023-10-20 19:05:28 +0600

    deployed in surge

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // Connect the client to the server	(optional starting in v4.7);
 
 
         const productCollection = client.db('brandShopDB').collection('brandShop')
